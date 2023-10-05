d973544a192ce6d4ecd2702e3560a477b67d6ec9
= <=>
2023-10-05 09:41:58 +0700

    ulatr

--- routes/socket.js
+++ routes/socket.js
@@ -120,9 +120,86 @@ module.exports = (socket) => {
         })
     })
     // not tested
-    socket.on("/dipe-production-new-data-added", (payload) => {
+    socket.on("/dipe-production-new-data-added", async (payload) => {
         const { data, api_id } = payload;
-        socket.broadcast.emit("/dipe-production-new-data-added", { data, api_id })
+        const apis = await Database.selectAll('apis', { api_id })
+        if (data && apis && apis[0]) {
+            const table_id = apis[0].tables[0]
+            const tables = await Database.selectAll("tables", { id: table_id })
+
+            const { statistic, calculates } = apis[0]
+
+            const table = tables[0]
+
+            const primaryKeys = table.primary_key;
+            const fields = await Database.selectAll("fields", { id: { $in: primaryKeys } })
+            const key = {}
+            for (let i = 0; i < fields.length; i++) {
+                const field = fields[i]
+                const { fomular_alias } = field;
+                key[fomular_alias] = data[fomular_alias]
+            }
+
+            const originData = await Database.selectAll( table.table_alias, key )
+            const record = originData[0]
+
+            if (calculates && calculates.length > 0) {
+                const keys = Object.keys(record)
+                keys.sort((key_1, key_2) => key_1.length > key_2.length ? 1 : -1);
+
+                for (let i = 0; i < calculates.length; i++) {
+                    const { fomular_alias, fomular } = calculates[i]
+                    let result = fomular;
+                    let originResult = fomular
+                    keys.map(key => {
+                        result = result.replaceAll(key, record[key])
+                        originResult = originResult.replaceAll(key, record[key])
+                    })
+                    try {
+                        record[fomular_alias] = eval(result)
+                    } catch {
+                        record[fomular_alias] = `${DEFAULT_ERROR_CALCLATED_VALUE}`;
+                    }
+                    try {
+                        record[fomular_alias] = eval(originResult)
+                    } catch {
+                        record[fomular_alias] = `${DEFAULT_ERROR_CALCLATED_VALUE}`;
+                    }
+                }
+            }
+
+            const sums = await Database.selectAll(table.table_alias, { position: "sumerize" })
+            const sumerize = sums[0]
+            const statistics = []
+            if (sumerize) {
+                statistic.map(statis => {
+                    const { display_name, fomular_alias, fomular, group_by } = statis;
+                    const statisRecord = { display_name }
+                    if (group_by && group_by.length > 0) {
+                        const rawData = sumerize[fomular_alias]
+                        if (rawData != undefined) {
+                            if (fomular == "AVERAGE") {
+                                const headers = Object.keys(rawData)
+                                const values = Object.values(rawData).map(({ total, value }) => value)
+
+                                statisRecord["data"] = { headers, values }
+                                statisRecord["type"] = "table"
+                            } else {
+                                const headers = Object.keys(rawData)
+                                const values = Object.values(rawData)
+                                statisRecord["data"] = { headers, values }
+                                statisRecord["type"] = "table"
+                            }
+                        }
+                    } else {
+                        statisRecord["type"] = "text"
+                        statisRecord["data"] = sumerize[fomular_alias]
+                    }
+                    statistics.push(statisRecord)
+                })
+            }            
+            socket.broadcast.emit("/dipe-production-new-data-added", { data: record, api_id, statistics })
+        }
     })
 
     socket.on("/dipe-production-delete-data", async (payload) => {
@@ -151,8 +228,9 @@ module.exports = (socket) => {
         const apis = await Database.selectAll('apis', { api_id })
         if (data && apis && apis[0]) {
             const table_id = apis[0].tables[0]
-            const tables = await Database.selectAll("tables", { id: table_id })
+            const { calculates, statistic } = apis[0]
 
+            const tables = await Database.selectAll("tables", { id: table_id })
             const table = tables[0]
 
             const primaryKeys = table.primary_key;
@@ -163,10 +241,66 @@ module.exports = (socket) => {
                 const { fomular_alias } = field;
                 key[fomular_alias] = data[fomular_alias]
             }
+            
+            const originData = await Database.selectAll( table.table_alias, key )
+            const record = originData[0]
 
-            console.log(key)
-            console.log(api_id)
-            socket.broadcast.emit("/dipe-production-update-data", { data, api_id, key })
+            if (calculates && calculates.length > 0) {
+                const keys = Object.keys(record)
+                keys.sort((key_1, key_2) => key_1.length > key_2.length ? 1 : -1);
+
+                for (let i = 0; i < calculates.length; i++) {
+                    const { fomular_alias, fomular } = calculates[i]
+                    let result = fomular;
+                    let originResult = fomular
+                    keys.map(key => {
+                        result = result.replaceAll(key, record[key])
+                        originResult = originResult.replaceAll(key, record[key])
+                    })
+                    try {
+                        record[fomular_alias] = eval(result)
+                    } catch {
+                        record[fomular_alias] = `${DEFAULT_ERROR_CALCLATED_VALUE}`;
+                    }
+                    try {
+                        record[fomular_alias] = eval(originResult)
+                    } catch {
+                        record[fomular_alias] = `${DEFAULT_ERROR_CALCLATED_VALUE}`;
+                    }
+                }
+            }
+
+            const sums = await Database.selectAll(table.table_alias, { position: "sumerize" })
+            const sumerize = sums[0]
+            const statistics = []
+            if (sumerize) {
+                statistic.map(statis => {
+                    const { display_name, fomular_alias, fomular, group_by } = statis;
+                    const statisRecord = { display_name }
+                    if (group_by && group_by.length > 0) {
+                        const rawData = sumerize[fomular_alias]
+                        if (rawData != undefined) {
+                            if (fomular == "AVERAGE") {
+                                const headers = Object.keys(rawData)
+                                const values = Object.values(rawData).map(({ total, value }) => value)
+
+                                statisRecord["data"] = { headers, values }
+                                statisRecord["type"] = "table"
+                            } else {
+                                const headers = Object.keys(rawData)
+                                const values = Object.values(rawData)
+                                statisRecord["data"] = { headers, values }
+                                statisRecord["type"] = "table"
+                            }
+                        }
+                    } else {
+                        statisRecord["type"] = "text"
+                        statisRecord["data"] = sumerize[fomular_alias]
+                    }
+                    statistics.push(statisRecord)
+                })
+            }
+            socket.broadcast.emit("/dipe-production-update-data", { data: record, api_id, key, statistics })
         }
     })
 
